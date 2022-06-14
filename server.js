1e3950a0aefbaefa7d97cf9bd0d7883685750c41
pramodbnadgeri <nadageripramod03@gmail.com>
2022-06-14 13:44:40 +0530

    server changed

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const PORT = process.env.PORT || 3000;
 // );
 
 app.listen(PORT, () => {
-    console.log("server running on" + PORT);
+    console.log("server running on " + PORT);
 });
 
 // const swaggerUi = require('swagger - ui - express'),
