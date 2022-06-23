4e54a5de83b5762993e4be34fab4a3df5a4da0ae
pramodbnadgeri <nadageripramod03@gmail.com>
2022-06-23 14:21:18 +0530

    seedin,config files added

--- config/config.js
+++ config/config.js
@@ -6,6 +6,9 @@ module.exports = {
     development: {
         url: process.env.DEV_DATABASE_URL,
         dialect: "postgres",
+        ssl: {
+            rejectUnauthorized: false
+        }
     },
     test: {
         url: process.env.TEST_DATABASE_URL,
