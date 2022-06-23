98ea84ed62b26bce7a892496ed0c62b30cfafac6
pramodbnadgeri <nadageripramod03@gmail.com>
2022-06-23 13:41:17 +0530

    config changes

--- models/index.js
+++ models/index.js
@@ -27,7 +27,7 @@ sequelize
     console.log("Connection has been established successfully.");
   })
   .catch((error) => {
-    // console.error("Unable to connect to the database:", error);
+    console.error("Unable to connect to the database:", error);
   });
 
 fs.readdirSync(__dirname)
